f420dc90230e342f8de917d13357f94c3f3bf22b
Melvina Gratiant <gratiant.m@gmail.com>
2021-01-24 21:21:33 +0100

    modification du type (string -> long) des donnees membres "siren" et "siret"

--- app/src/main/java/fr/esimed/search_company/data/model/SearchResult.kt
+++ app/src/main/java/fr/esimed/search_company/data/model/SearchResult.kt
@@ -11,8 +11,8 @@ import androidx.room.PrimaryKey
 
 data class SearchResult(@PrimaryKey(autoGenerate = true) var id:Long? = null,
                         var company_corporate_name:String,
-                        var siren:String,
-                        var siret:String,
+                        var siren:Long,
+                        var siret:Long,
                         var created_date:String,
                         var company_category:String,
                         var id_search_company:Long)
