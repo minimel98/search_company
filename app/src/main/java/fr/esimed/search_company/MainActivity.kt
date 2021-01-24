bd8f41cc7886d2c798679c314b4b257c011e8f93
Melvina Gratiant <gratiant.m@gmail.com>
2021-01-24 21:20:17 +0100

    recuperation de la bd + appel a la fonction seed

--- app/src/main/java/fr/esimed/search_company/MainActivity.kt
+++ app/src/main/java/fr/esimed/search_company/MainActivity.kt
@@ -2,11 +2,16 @@ package fr.esimed.search_company
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fr.esimed.search_company.data.dao.SearchcompanyDatabase
 
 class MainActivity : AppCompatActivity()
 {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = SearchcompanyDatabase.getDatabase(this)
+        db.seed()
     }
 }
