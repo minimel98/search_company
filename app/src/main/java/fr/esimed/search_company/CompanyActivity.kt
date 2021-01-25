dd83402e6ee7d10da784637948433ed1536c07e1
Melvina Gratiant <gratiant.m@gmail.com>
2021-01-25 17:07:47 +0100

    ajout d'une nouvelle activité + ajout d'un fichier ressources xml pour les dimensions + ajout de string dans le fichier ressource strings

---
+++ app/src/main/java/fr/esimed/search_company/CompanyActivity.kt
@@ -0,0 +1,11 @@
+package fr.esimed.search_company
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CompanyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_company)
+    }
+}
